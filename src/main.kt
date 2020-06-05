e851389d339828aa864f29ba434f3f56ed20c094
Rahul_Rao <rahulraoe21@gmail.com>
2020-06-05 17:39:27 +0530

    Kotlin if and when

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,25 @@
 fun main(args : Array<String>){
     println("Hello World")
-
+var age= readLine()!!.toInt()
+    var message:String
+    message=if(age>18){
+        "you can vote"
+    }else if(age<18){
+        "you cannot vote"
+    }
+    else
+        "you can vote"
+    println(message)
+    println("Enter age again")
+     age= readLine()!!.toInt()
+    message= when {
+        age>18 -> {
+            "you can vote"
+        }
+        age<18 -> {
+            "you cannot vote"
+        }
+        else -> "you can vote"
+    }
+    println(message)
 }
